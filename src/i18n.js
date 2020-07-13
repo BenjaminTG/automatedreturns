b903b5e3df77c034a39c95cfa20b8b8b6e37972c
Belen Jones <bjones@brandcollective.com.au>
2020-07-13 13:56:53 +1000

    i18n

--- src/i18n.js
+++ src/i18n.js
@@ -5,8 +5,8 @@
 
 // export default t
 
-import { initReactI18next } from "react-i18next";
-import i18n from "i18next";
+import { initReactI18next } from 'react-i18next';
+import i18n from './i18n.js';
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
